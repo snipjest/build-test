27aa73cec9ece6cbe6222ec1c117aa9c635bc366
Dmitry Katkov <kadimsnip@gmail.com>
2025-06-30 15:35:11 +0300

    arhitektor

--- plugins/init.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Pinia } from 'pinia'
-import { useStoreLayout } from '@/stores/useStoreLayout'
-
-export default defineNuxtPlugin(async nuxtApp => {
-  const pinia = nuxtApp.$pinia as Pinia
-  const layout = useStoreLayout(pinia)
-  await layout.fetch()
-})
