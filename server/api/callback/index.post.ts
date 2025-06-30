14f10ded653f75f0d19f561bf86f9a24c763df13
Dmitry Katkov <kadimsnip@gmail.com>
2025-06-30 14:26:47 +0300

    arhitektor

--- server/api/callback/index.post.ts
+++ server/api/callback/index.post.ts
@@ -1,17 +1,3 @@
 export default defineEventHandler(async event => {
-  const config = useRuntimeConfig()
-  const baseUrl = config.public.baseUrl
-  const body = await readBody(event)
 
-  // Если baseUrl не задан
-  if (!baseUrl) {
-    // return console.log(body)
-  }
-
-  // Иначе — запрос к удалённому серверу
-  const response: any = await $fetch(`${baseUrl}/api/callback/`, {
-    method: 'POST',
-    body
-  })
-  return response
 })
